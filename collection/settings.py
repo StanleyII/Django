eae7cdc3ff70515daed4be41f0485dc6442f8874
Nazarko A. <pa6om9lra@gmail.com>
2024-08-21 11:18:10 +0300

    Дополнил данными каталог и добавил стартовые шаблоны приложений (startpage, users)

--- collection/settings.py
+++ collection/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
     'base.apps.BaseConfig',
+    'users.apps.UsersConfig',
+    'startpage.apps.StartpageConfig',
 ]
 
 MIDDLEWARE = [
